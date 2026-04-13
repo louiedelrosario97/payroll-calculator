083631113fbd78cc0cf8410c94f4259c3756f34e
Louie D <louie.delrosario97@gmail.com>
2026-04-12 18:41:33 -0700

    Completed main code. Next I need to work on the optional activity to add overtime pay.

--- src/PayrollCalculator.java
+++ src/PayrollCalculator.java
@@ -7,14 +7,15 @@ public class PayrollCalculator {
     System.out.print ("Enter Name: "); //prompt for name entry
     String name = scan.nextLine();
 
-    System.out.print ("Enter total hours: "); //prompt for total hours entry
+    System.out.print ("Enter total hours worked this year: "); //prompt for total hours entry
     double hours = scan.nextDouble();
 
     System.out.print ("Enter pay rate: "); //prompt got pay rate entry
     double payRate = scan.nextDouble();
 
-    //This is a method call, we are calling the 'totalGrossPay' method, and sending the 'hours' and 'payRate' inputs
-    double grossPay = totalGrossPay(hours, payRate);
+    double grossPay = (hours * payRate);
+
+    System.out.println("Hey " + name + ", you only made $" + grossPay + " in gross pay this year. Yikes.");
 
 
     }
